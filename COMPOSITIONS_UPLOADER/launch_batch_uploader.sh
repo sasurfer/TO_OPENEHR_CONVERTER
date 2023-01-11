be90944e41726fa7d00e3f0dda2727ab6bd8cf18
Giovanni Delussu <surfer@crs4.it>
2023-01-11 11:33:27 +0100

    2022 cleanest version used to fill ehrbase

--- COMPOSITIONS_UPLOADER/launch_batch_uploader.sh
+++ COMPOSITIONS_UPLOADER/launch_batch_uploader.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 mya1="--inputdir=/usr/local/data/WORK/OPENEHR/ECOSYSTEM/TO_AND_FROM_CONVERTER/CODE/RESULTS/"
-mya2="--templatename=mycrc_cohort6mod"
+mya2="--templatename=crc_cohort7"
 mya3="--check"
 mya4="--loglevel=INFO"
-myb1="--basename=myoutputceciDB1_"
-myb2="--basename=myoutputceciDB2_"
-myb3="--basename=myoutputceciDB3_"
-myb4="--basename=myoutputceciDB4_"
-myb5="--basename=myoutputceciDB5_"
-myb6="--basename=myoutputceciDB6_"
-myb7="--basename=myoutputceciDB7_"
-myb8="--basename=myoutputceciDB8_"
-myb9="--basename=myoutputceciDB9_"
-myb10="--basename=myoutputceciDB10_"
-myb11="--basename=myoutputceciDB11_"
-python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb1  > uploadingresultsceciDB1
-python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb2  > uploadingresultsceciDB2
-python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb3  > uploadingresultsceciDB3
-python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb4  > uploadingresultsceciDB4
-python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb5  > uploadingresultsceciDB5
-python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb6  > uploadingresultsceciDB6
-python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb7  > uploadingresultsceciDB7
-python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb8  > uploadingresultsceciDB8
-python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb9  > uploadingresultsceciDB9
-python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb10 > uploadingresultsceciDB10
-python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb11 > uploadingresultsceciDB11
+myb1="--basename=myoutput7DB1_"
+myb2="--basename=myoutput7DB2_"
+myb3="--basename=myoutput7DB3_"
+myb4="--basename=myoutput7DB4_"
+myb5="--basename=myoutput7DB5_"
+myb6="--basename=myoutput7DB6_"
+myb7="--basename=myoutput7DB7_"
+myb8="--basename=myoutput7DB8_"
+myb9="--basename=myoutput7DB9_"
+myb10="--basename=myoutput7DB10_"
+myb11="--basename=myoutput7DB11_"
+python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb1  > uploadingresults7DB1
+python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb2  > uploadingresults7DB2
+python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb3  > uploadingresults7DB3
+python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb4  > uploadingresults7DB4
+python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb5  > uploadingresults7DB5
+python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb6  > uploadingresults7DB6
+python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb7  > uploadingresults7DB7
+python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb8  > uploadingresults7DB8
+python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb9  > uploadingresults7DB9
+python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb10 > uploadingresults7DB10
+python3 ./CompositionUploader.py $mya1 $mya2 $mya3 $mya4 $myb11 > uploadingresults7DB11
--- launch_batch_code.sh
+++ launch_batch_code.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-mya1="--webtemplate=/usr/local/data/WORK/OPENEHR/ECOSYSTEM/TO_AND_FROM_CONVERTER/CODE/RESULTS/TEST/NEWTEMPLATE/mycrc_cohort6mod.json"
+mya1="--webtemplate=/usr/local/data/WORK/OPENEHR/ECOSYSTEM/TO_AND_FROM_CONVERTER/CODE/RESULTS/TEST/crc_cohort7.json"
 mya2="--ehrbase"
 mya3="--check"
 mya4="--loglevel=INFO"
-myb1="--outputfilebasename=myoutputceciDB1"
-myb2="--outputfilebasename=myoutputceciDB2"
-myb3="--outputfilebasename=myoutputceciDB3"
-myb4="--outputfilebasename=myoutputceciDB4"
-myb5="--outputfilebasename=myoutputceciDB5"
-myb6="--outputfilebasename=myoutputceciDB6"
-myb7="--outputfilebasename=myoutputceciDB7"
-myb8="--outputfilebasename=myoutputceciDB8"
-myb9="--outputfilebasename=myoutputceciDB9"
-myb10="--outputfilebasename=myoutputceciDB10"
-myb11="--outputfilebasename=myoutputceciDB11"
+myb1="--outputfilebasename=myoutput7DB1"
+myb2="--outputfilebasename=myoutput7DB2"
+myb3="--outputfilebasename=myoutput7DB3"
+myb4="--outputfilebasename=myoutput7DB4"
+myb5="--outputfilebasename=myoutput7DB5"
+myb6="--outputfilebasename=myoutput7DB6"
+myb7="--outputfilebasename=myoutput7DB7"
+myb8="--outputfilebasename=myoutput7DB8"
+myb9="--outputfilebasename=myoutput7DB9"
+myb10="--outputfilebasename=myoutput7DB10"
+myb11="--outputfilebasename=myoutput7DB11"
 myc1="--inputfile=/usr/local/data/WORK/OPENEHR/ECOSYSTEM/TO_AND_FROM_CONVERTER/CODE/FROM_DB_CSV_TO_XML_CONVERTER/patientsFromDb_1.xml"
 myc2="--inputfile=/usr/local/data/WORK/OPENEHR/ECOSYSTEM/TO_AND_FROM_CONVERTER/CODE/FROM_DB_CSV_TO_XML_CONVERTER/patientsFromDb_2.xml"
 myc3="--inputfile=/usr/local/data/WORK/OPENEHR/ECOSYSTEM/TO_AND_FROM_CONVERTER/CODE/FROM_DB_CSV_TO_XML_CONVERTER/patientsFromDb_3.xml"
@@ -25,14 +25,14 @@ myc8="--inputfile=/usr/local/data/WORK/OPENEHR/ECOSYSTEM/TO_AND_FROM_CONVERTER/C
 myc9="--inputfile=/usr/local/data/WORK/OPENEHR/ECOSYSTEM/TO_AND_FROM_CONVERTER/CODE/FROM_DB_CSV_TO_XML_CONVERTER/patientsFromDb_9.xml"
 myc10="--inputfile=/usr/local/data/WORK/OPENEHR/ECOSYSTEM/TO_AND_FROM_CONVERTER/CODE/FROM_DB_CSV_TO_XML_CONVERTER/patientsFromDb_10.xml"
 myc11="--inputfile=/usr/local/data/WORK/OPENEHR/ECOSYSTEM/TO_AND_FROM_CONVERTER/CODE/FROM_DB_CSV_TO_XML_CONVERTER/patientsFromDb_11.xml"
-python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb1 $myc1 > conversion.screen.ceciDB1
-python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb2 $myc2 > conversion.screen.ceciDB2
-python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb3 $myc3 > conversion.screen.ceciDB3
-python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb4 $myc4 > conversion.screen.ceciDB4
-python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb5 $myc5 > conversion.screen.ceciDB5
-python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb6 $myc6 > conversion.screen.ceciDB6
-python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb7 $myc7 > conversion.screen.ceciDB7
-python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb8 $myc8 > conversion.screen.ceciDB8
-python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb9 $myc9 > conversion.screen.ceciDB9
-python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb10 $myc10> conversion.screen.ceciDB10
-python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb11 $myc11> conversion.screen.ceciDB11
+python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb1 $myc1 > conversion.screen7DB1
+python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb2 $myc2 > conversion.screen7DB2
+python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb3 $myc3 > conversion.screen7DB3
+python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb4 $myc4 > conversion.screen7DB4
+python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb5 $myc5 > conversion.screen7DB5
+python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb6 $myc6 > conversion.screen7DB6
+python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb7 $myc7 > conversion.screen7DB7
+python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb8 $myc8 > conversion.screen7DB8
+python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb9 $myc9 > conversion.screen7DB9
+python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb10 $myc10> conversion.screen7DB10
+python3 ./main.py $mya1 $mya2 $mya3 $mya4 $myb11 $myc11> conversion.screen7DB11
